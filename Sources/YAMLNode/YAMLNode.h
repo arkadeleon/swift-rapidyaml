b7acfea86f169611d8dd80047225b0a4abd1b052
Leon Li <arkadeleon@gmail.com>
2026-04-29 18:18:45 +0800

    Added rich node inspection properties and child traversal to YAMLNode

--- Sources/YAMLNode/YAMLNode.h
+++ Sources/YAMLNode/YAMLNode.h
@@ -9,7 +9,44 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM(NSInteger, YAMLNodeKind) {
+    YAMLNodeKindUnknown = 0,
+    YAMLNodeKindStream,
+    YAMLNodeKindDocument,
+    YAMLNodeKindMapping,
+    YAMLNodeKindSequence,
+    YAMLNodeKindScalar,
+} NS_SWIFT_NAME(YAMLNode.Kind);
+
 @interface YAMLNode : NSObject
+
+@property (nonatomic, readonly) YAMLNodeKind kind;
+@property (nonatomic, readonly) uint32_t typeBits;
+@property (nonatomic, readonly, copy) NSString *typeString;
+
+@property (nonatomic, readonly) BOOL isStream;
+@property (nonatomic, readonly) BOOL isDoc;
+@property (nonatomic, readonly) BOOL isMap;
+@property (nonatomic, readonly) BOOL isSeq;
+@property (nonatomic, readonly) BOOL isContainer;
+@property (nonatomic, readonly) BOOL hasKey;
+@property (nonatomic, readonly) BOOL hasValue;
+@property (nonatomic, readonly) BOOL isNull;
+@property (nonatomic, readonly) BOOL isRef;
+@property (nonatomic, readonly) BOOL hasAnchor;
+
+@property (nonatomic, readonly, copy, nullable) NSString *key;
+@property (nonatomic, readonly, copy, nullable) NSString *value;
+@property (nonatomic, readonly, copy, nullable) NSString *keyTag;
+@property (nonatomic, readonly, copy, nullable) NSString *valueTag;
+@property (nonatomic, readonly, copy, nullable) NSString *keyAnchor;
+@property (nonatomic, readonly, copy, nullable) NSString *valueAnchor;
+@property (nonatomic, readonly, copy, nullable) NSString *keyReference;
+@property (nonatomic, readonly, copy, nullable) NSString *valueReference;
+
+@property (nonatomic, readonly, weak, nullable) YAMLNode *parent;
+@property (nonatomic, readonly, copy) NSArray<YAMLNode *> *children;
+@property (nonatomic, readonly) NSUInteger childCount;
 
 @property (nonatomic, readonly, copy, nullable) NSDictionary<NSString *, YAMLNode *> *mapping;
 @property (nonatomic, readonly, copy, nullable) NSArray<YAMLNode *> *sequence;
@@ -17,6 +54,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (instancetype)init NS_UNAVAILABLE;
 - (instancetype)initWithYAMLString:(NSString *)yamlString;
+
+- (nullable YAMLNode *)childAtIndex:(NSUInteger)index NS_SWIFT_NAME(child(at:));
+- (nullable YAMLNode *)childForKey:(NSString *)key NS_SWIFT_NAME(child(forKey:));
 
 @end
 
--- Sources/YAMLNode/YAMLNode.mm
+++ Sources/YAMLNode/YAMLNode.mm
@@ -8,45 +8,135 @@
 #import "YAMLNode.h"
 #import "ryml/ryml.hpp"
 
-NSString *NSStringFromSubstr(c4::csubstr substr) {
+static NSString * _Nullable NSStringFromSubstr(c4::csubstr substr) {
+    if (substr.str == nullptr) {
+        return nil;
+    }
+
     NSData *data = [[NSData alloc] initWithBytes:substr.str length:substr.len];
     NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     return string;
+}
+
+static YAMLNodeKind YAMLNodeKindFromNode(ryml::ConstNodeRef node) {
+    if (node.is_stream()) {
+        return YAMLNodeKindStream;
+    }
+    if (node.is_doc()) {
+        return YAMLNodeKindDocument;
+    }
+    if (node.is_map()) {
+        return YAMLNodeKindMapping;
+    }
+    if (node.is_seq()) {
+        return YAMLNodeKindSequence;
+    }
+    if (node.has_val()) {
+        return YAMLNodeKindScalar;
+    }
+    return YAMLNodeKindUnknown;
 }
 
 @implementation YAMLNode
 
 - (instancetype)initWithYAMLString:(NSString *)yamlString {
     ryml::Tree tree = ryml::parse_in_arena([yamlString UTF8String]);
-    return [self initWithNode:tree.rootref()];
+    return [self initWithNode:tree.rootref() parent:nil];
 }
 
-- (instancetype)initWithNode:(ryml::NodeRef)node {
+- (instancetype)initWithNode:(ryml::ConstNodeRef)node parent:(nullable YAMLNode *)parent {
     self = [super init];
     if (self) {
+        _parent = parent;
+        _kind = YAMLNodeKindFromNode(node);
+        _typeBits = static_cast<uint32_t>(node.type().type);
+        _typeString = [NSString stringWithUTF8String:node.type_str()];
+
+        _isStream = node.is_stream();
+        _isDoc = node.is_doc();
+        _isMap = node.is_map();
+        _isSeq = node.is_seq();
+        _isContainer = node.is_container();
+        _hasKey = node.has_key();
+        _hasValue = node.has_val();
+        _isNull = node.has_val() && node.val_is_null();
+        _isRef = node.is_ref();
+        _hasAnchor = node.has_anchor();
+
+        if (node.has_key()) {
+            _key = NSStringFromSubstr(node.key());
+        }
+        if (node.has_val()) {
+            _value = NSStringFromSubstr(node.val());
+        }
+        if (node.has_key_tag()) {
+            _keyTag = NSStringFromSubstr(node.key_tag());
+        }
+        if (node.has_val_tag()) {
+            _valueTag = NSStringFromSubstr(node.val_tag());
+        }
+        if (node.has_key_anchor()) {
+            _keyAnchor = NSStringFromSubstr(node.key_anchor());
+        }
+        if (node.has_val_anchor()) {
+            _valueAnchor = NSStringFromSubstr(node.val_anchor());
+        }
+        if (node.is_key_ref()) {
+            _keyReference = NSStringFromSubstr(node.key_ref());
+        }
+        if (node.is_val_ref()) {
+            _valueReference = NSStringFromSubstr(node.val_ref());
+        }
+
+        NSMutableArray<YAMLNode *> *children = [NSMutableArray array];
+        size_t childCount = node.num_children();
+        for (size_t pos = 0; pos < childCount; pos++) {
+            auto child = node.child(pos);
+            YAMLNode *childNode = [[YAMLNode alloc] initWithNode:child parent:self];
+            [children addObject:childNode];
+        }
+        _children = [children copy];
+        _childCount = _children.count;
+
         if (node.is_map()) {
             NSMutableDictionary<NSString *, YAMLNode *> *mapping = [NSMutableDictionary dictionary];
-            size_t childCount = node.num_children();
-            for (size_t pos = 0; pos < childCount; pos++) {
-                auto child = node.child(pos);
-                NSString *key = NSStringFromSubstr(child.key());
-                mapping[key] = [[YAMLNode alloc] initWithNode:child];
+            for (YAMLNode *child in _children) {
+                if (child.key != nil) {
+                    mapping[child.key] = child;
+                }
             }
             _mapping = [mapping copy];
-        } else if (node.is_seq()) {
-            NSMutableArray<YAMLNode *> *sequence = [NSMutableArray array];
-            size_t childCount = node.num_children();
-            for (size_t pos = 0; pos < childCount; pos++) {
-                auto child = node.child(pos);
-                YAMLNode *childNode = [[YAMLNode alloc] initWithNode:child];
-                [sequence addObject:childNode];
-            }
-            _sequence = [sequence copy];
-        } else if (node.is_keyval() && !node.val_is_null()) {
+        }
+
+        if (node.is_seq()) {
+            _sequence = _children;
+        }
+
+        if (!node.is_container() && node.has_val() && !node.val_is_null()) {
             _scalar = NSStringFromSubstr(node.val());
+        }
+
+        if (_children == nil) {
+            _children = @[];
         }
     }
     return self;
+}
+
+- (nullable YAMLNode *)childAtIndex:(NSUInteger)index {
+    if (index >= self.children.count) {
+        return nil;
+    }
+    return self.children[index];
+}
+
+- (nullable YAMLNode *)childForKey:(NSString *)key {
+    for (YAMLNode *child in self.children) {
+        if ([child.key isEqualToString:key]) {
+            return child;
+        }
+    }
+    return nil;
 }
 
 @end
